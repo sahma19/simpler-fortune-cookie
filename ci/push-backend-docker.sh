9bb9d5fc5eef90a387ff8e4b5149419db5eb5a0b
sahma19 <55732914+sahma19@users.noreply.github.com>
2021-08-24 00:15:51 +0200

    Update push-backend-docker.sh

--- ci/push-backend-docker.sh
+++ ci/push-backend-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/fortune-app-backend:1.0-${GIT_COMMIT::4}" 
-docker push "$docker_username/fortune-app-backend:latest" &
+docker push "$docker_username/simpler-fortune-cookie-app-backend:1.0-${GIT_COMMIT::4}" 
+docker push "$docker_username/simpler-fortune-cookie-app-backend:latest" &
 wait
