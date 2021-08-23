f2f26094459318bc639070a492d30416f690e942
sahma19 <55732914+sahma19@users.noreply.github.com>
2021-08-24 00:12:54 +0200

    Update build-backend-docker.sh

--- ci/build-backend-docker.sh
+++ ci/build-backend-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}fortune-app-backend:latest" -t "${DockerRepo}fortune-app-backend:1.0-$Tag" backend/
+docker build -t "${DockerRepo}simpler-fortune-cookie-app-backend:latest" -t "${DockerRepo}simpler-fortune-cookie-app-backend:1.0-$Tag" backend/
